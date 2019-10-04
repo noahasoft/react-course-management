0359711606365d0ecc57f16af0268fd95fe97bbc
Noah Baxter <noahasoft@gmail.com>
2019-10-04 15:05:43 -0700

    Added React Router with 404 error page.

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,22 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import CoursesPage from './CoursesPage';
 import Header from './common/Header';
+import NotFoundPage from './NotFoundPage';
+import ManageCoursePage from './ManageCoursePage';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
-  const route = window.location.pathname;
-
-  function getPage() {
-    if (route === '/courses') return <CoursesPage />;
-    if (route === '/about') return <AboutPage />;
-    return <HomePage />;
-  }
-
   return (
     <div className="container-fluid">
       <Header />
-      {getPage()}
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Redirect from="/about-page" to="/about" />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }
